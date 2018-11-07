402da605c815eef01c026eee4402c89a7a9ec627
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-11-07 10:39:54 +0100

    Working GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 
@@ -9,11 +10,14 @@ public class Controller {
 
     @FXML
     public TextField commandFieldx;
+    public TextArea textArea;
 
     public void CommandButton(ActionEvent actionEvent) {
-       String s = commandFieldx.getText();
-        System.out.println("Klik" + s);
-    }
+        String s = commandFieldx.getText();
+        System.out.println("Klik " + s);
+        commandFieldx.clear();
+        textArea.setText("Der er indtastet: " + s);
 
+    }
 
 }
