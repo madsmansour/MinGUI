e178537e33fc40b36b3a0e82520e947ff6b78677
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-11-07 10:52:07 +0100

    Working GUI with comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,17 +8,25 @@ import javafx.scene.control.TextField;
 
 public class Controller {
 
+// bruger scenecontrol til text field og text area
     @FXML
     public TextField commandFieldx;
     public TextArea textArea;
 
+    // En metode til knappen CommandButton
     public void CommandButton(ActionEvent actionEvent) {
-        String s = commandFieldx.getText();
-        System.out.println("Klik " + s);
-        commandFieldx.clear();
-        textArea.setText(textArea.getText() + "Der er indtastet: " + s + "\n");
 
+        // laver en string variabel s som tager teksten fra textfieldet
+            String s = commandFieldx.getText();
+
+            System.out.println("Klik " + s);
+
+            // Clearer textfieldet efter hvert input
+            commandFieldx.clear();
+
+            // får textarea til at hente text fra string s, samt lave en ny linje hvor næste input bliver sat ind.
+            textArea.setText(textArea.getText() + "Der er indtastet: " + s + "\n");
+
+        }
 
     }
-
-}
